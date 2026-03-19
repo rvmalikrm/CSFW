17d22ea1dc6e3dc96b9127bec34e21b97e23a976
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-19 15:44:32 +0530

    Push code for Identifier, Literal Operators and expressions

---
+++ Chapter_01_Basics/Chapter_02_JS_concepts/05_Core_identifier_JS.js
@@ -0,0 +1,13 @@
+// Identifier, Literal, Operators, Expressions, Keywords
+var _a = 10; // _a is an identifier, 10 is a literal
+var $b = 20;
+console.log(_a + $b); // _a and $b are identifiers, + is an operator, _a + $b is an expression
+
+// Keywords: Reserved words in JavaScript that have a specific meaning and cannot be used as identifiers. 
+// Examples: var, let, const, function, if, else, return, etc.
+
+// Identifier: A name used to identify a variable, function, or property in JavaScript.
+// It must start with a letter, underscore (_), or dollar sign ($) and can be followed by letters, digits, underscores, or dollar signs. Examples: myVariable, _myFunction, $myProperty.
+
+// Literal: A fixed value that is directly represented in the code. 
+// Examples: 42 (number literal), "Hello" (string literal),true (boolean literal).
---
+++ Chapter_01_Basics/Chapter_02_JS_concepts/05_core_comments_javascript.js
@@ -0,0 +1,16 @@
+// This is the comment in JavaScript it will not execute
+
+console.log("Hello World"); 
+
+/** 
+ * This is multi line conmment in JavaScript
+ * It will not execute
+ * It is used to write multiple lines of comments
+ **/
+
+/*
+ * This is multi line conmment in JavaScript
+ * It will not execute
+ * It is used to write multiple lines of comments
+ */
+
