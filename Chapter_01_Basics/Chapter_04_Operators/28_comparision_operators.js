72378df73cbe51981096e770d06b15aaca7da598
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-20 17:33:15 +0530

    Comparision Operators

---
+++ Chapter_01_Basics/Chapter_04_Operators/28_comparision_operators.js
@@ -0,0 +1,11 @@
+// =  , ==, ===, !=, !==, >, <, >=, <=
+
+console.log(1 == 1);
+
+
+// =  is an assignment operator, it assigns the value on the right to the variable on the left.
+// == is a comparison operator that checks for equality of values, it performs type coercion if necessary.
+// === is a strict equality operator that checks for equality of both value and type, it does not perform type coercion.
+
+
+// >,<, !=, <=,>=, 
