9a340e34e1c9c48b381da36fd2afa4b71996cc07
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-20 17:40:37 +0530

    Ternary Operator

---
+++ Chapter_01_Basics/Chapter_04_Operators/31_Ternary_Operators.js
@@ -0,0 +1,8 @@
+let age =20;
+
+let is_Ravi_go_to_GOA= age >= 18 ? "Yes, Ravi can go to Goa" : "No, Ravi cannot go to Goa";
+console.log(is_Ravi_go_to_GOA); // Output: "Yes, Ravi can go to Goa"
+
+let number = 10;
+let isEven = number % 2 === 0 ? "Even" : "Odd";
+console.log(isEven); // Output: "Even"
