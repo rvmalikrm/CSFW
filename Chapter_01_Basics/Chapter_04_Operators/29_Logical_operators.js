74188a6dd739a64967a42d5c2c57ed892ebdedc9
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-20 17:37:37 +0530

    Logical Operators

---
+++ Chapter_01_Basics/Chapter_04_Operators/29_Logical_operators.js
@@ -0,0 +1,15 @@
+// && --> AND Gate  
+
+// || --> OR Gate
+
+
+// OR Gate  
+
+let a = true;
+let b = false;
+console.log(a || b); // true (at least one operand is true)
+console.log(a && b); // false (both operands must be true)
+console.log(!a);
+console.log(!b);    
+
+
