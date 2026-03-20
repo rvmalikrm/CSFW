95fbebb732a206a8b1b466979c15938fe1f3718f
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-20 17:45:08 +0530

    Type Operator

---
+++ Chapter_01_Basics/Chapter_04_Operators/32_Type_Operators.js
@@ -0,0 +1,10 @@
+console.log(typeof 42); // Output: "number"
+console.log(typeof 3.14); // Output: "number"
+console.log(typeof "Hello"); // Output: "string"
+console.log(typeof true); // Output: "boolean"
+console.log(typeof undefined); // Output: "undefined"
+console.log(typeof null); // Output: "object" (this is a quirk in JavaScript, null is considered an object)
+console.log(typeof {}); // Output: "object"
+console.log(typeof []); // Output: "object" (arrays are also considered objects in JavaScript)
+
+// Typeof operator is used to determine the type of a variable or an expression. It returns a string indicating the type of the operand.
