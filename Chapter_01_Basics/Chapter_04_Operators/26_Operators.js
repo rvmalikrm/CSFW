fb81168fff1e77f16df757c22ba61c74d008d7d1
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-20 17:23:23 +0530

    Assignment Operators

---
+++ Chapter_01_Basics/Chapter_04_Operators/26_Operators.js
@@ -0,0 +1,16 @@
+let a=10, b=3;
+console.log(a);
+console.log(b);
+// Arthmetic operators
+let sum = a + b;
+console.log(sum); // Addition
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+//===================Extra
+
+console.log(a%b); // Modulus operator gives the remainder of the division of a by b
+console.log(a**b); // Exponentiation operator raises a to the power of b
---
+++ Chapter_01_Basics/Chapter_04_Operators/27_AssignmentOperators.js
@@ -0,0 +1,11 @@
+let a=10;
+a += 5; // Equivalent to a = a + 5
+console.log(a);
+a-= 3; // Equivalent to a = a - 3
+console.log(a);
+a*= 2;
+console.log(a); // Output: 24 (10 + 5 - 3) * 2
+a /= 4;
+console.log(a); // Output: 6 (24 / 4)
+a %= 4;
+console.log(a); // Output: 2 (6 % 4)
