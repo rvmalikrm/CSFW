d7db38c58c788006e347ac787d28e6c89abae25f
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-27 07:56:43 +0530

    35 Statements

--- Chapter_01_Basics/Chapter_05_Statements/35_Statement1.js
+++ Chapter_01_Basics/Chapter_05_Statements/35_Statement1.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+let age =24;
+
+if(age>=25){
+    console.log("You can go Goa and Drink beer");
+}
+else if(age>=18 && age<25){
+    console.log("You can go Goa but you can't drink beer");
+
+}else{
+    console.log("You can't go Goa");
+}
