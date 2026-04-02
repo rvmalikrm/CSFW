4f13add25094b1a93bcc6ad4ee54e4a74d5087ef
Ravi Malik <rm.rvmalik@gmail.com>
2026-04-02 22:50:33 +0530

    Fizz Buzz

---
+++ JSPractice/Chapter_3/02_Fiz_Buzz.js
@@ -0,0 +1,14 @@
+for(let i=1;i<=100;i++){
+    if(i%3===0 && i%5===0){
+        console.log(i+" is FizzBuzz");
+    }else if(i%3){
+        console.log(i+" is Fizz")
+    }
+    else if(i%5){
+        console.log(i+" is Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
+    
