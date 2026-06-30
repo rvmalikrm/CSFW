4262c1894407293a133fcb6744f92b45371aad25
rvmalikrm <rm.rvmalik@gmail.com>
2026-06-30 14:26:18 +0530

    example 5

---
+++ Chapter_01_Basics/Chapter_16_OOPs/163_Example5.js
@@ -0,0 +1,20 @@
+class Token {
+  #value;
+
+  constructor(val) {
+    this.#value = val;
+  }
+
+  getToken() {
+    return this.#value;
+  }
+
+  getMasked() {
+    return "***" + this.#value.slice(-3);
+  }
+}
+
+let t = new Token("abcdef1234");
+console.log(t.getMasked());
+console.log(t.getToken());
+console.log(t.value);
