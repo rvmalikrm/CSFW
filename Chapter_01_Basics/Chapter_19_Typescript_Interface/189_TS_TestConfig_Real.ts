eee171abb164ad4ba70eab4dcaf3a16e41df0451
rvmalikrm <rm.rvmalik@gmail.com>
2026-07-08 17:01:50 +0530

    189 interface example

---
+++ Chapter_01_Basics/Chapter_19_Typescript_Interface/189_TS_TestConfig_Real.ts
@@ -0,0 +1,24 @@
+interface TestConfig {
+    browser:string;
+    headless:boolean;
+    timeout?:number;
+    baseUrl:string;
+    retries?:number;
+}
+
+let ciConfig: Testconfig = {
+    browser: "chrome",
+    headless: true,
+    baseUrl: "https://staging.app.com",
+};
+
+let localConfig:TestConfig= {
+    browser: "firefox",
+    headless: false,
+    timeout: 5000,
+    baseUrl: "http://localhost:3000",
+    retries: 2  
+}
+
+console.log("CI",ciConfig.browser, " timeout :", ciConfig.timeout, " retries :", ciConfig.retries);
+console.log("Local",localConfig.browser, " timeout :", localConfig.timeout, " retries :", localConfig.retries   );
